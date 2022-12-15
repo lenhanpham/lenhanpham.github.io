afca35e08ce6fc552d345b02104fc156de6a28d6
Le Nhan Pham <lenhanpham@tutanota.com>
2022-12-15 16:06:32 +1100

    2022-12-15

--- fast-git.sh
+++ fast-git.sh
@@ -2,3 +2,6 @@ date=$(date '+%Y-%m-%d')
 git add .
 git commit -m "$date"
 git push origin master 
+
+# token: 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
+# If error happens or login required, username and password of github should be used to fill the box, and for the second time username and token will be used.
